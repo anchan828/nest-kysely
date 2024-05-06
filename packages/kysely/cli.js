d018c180f1b14152f6960672911e055a882909e2
anchan828 <anchan828@gmail.com>
2024-05-06 22:36:22 +0900

    chore: use parseArgs

--- packages/kysely/cli.js
+++ packages/kysely/cli.js
@@ -2,7 +2,25 @@
 
 const { relative, join } = require("node:path");
 const { writeFileSync, mkdirSync } = require("node:fs");
+const { parseArgs } = require("node:util");
 const args = process.argv.slice(2);
+const parsed = parseArgs({
+  args,
+  allowPositionals: true,
+  options: {
+    type: {
+      type: "string",
+      short: "t",
+      default: "ts",
+      multiple: false,
+    },
+    "no-down": {
+      type: "boolean",
+      default: false,
+      multiple: false,
+    },
+  },
+});
 
 function errorText(text) {
   return `\u001b[31m${text}\u001b[0m`;
@@ -10,14 +28,14 @@ function errorText(text) {
 
 const migrationCommandName = "migration:create";
 
-const usage = `Usage: nest-kysely ${migrationCommandName} <migration-directory> <migration-filename>`;
+const usage = `Usage: nest-kysely ${migrationCommandName} [--type {ts,sql}] [--no-down] <migration-directory> <migration-filename>`;
 
-const commandName = args[0];
-const migrationDir = args[1];
-const migrationName = args[2]?.replace(/\s+/g, "");
+const commandName = parsed.positionals[0];
+const migrationDir = parsed.positionals[1];
+const migrationName = parsed.positionals[2]?.replace(/\s+/g, "");
 
 if (commandName !== migrationCommandName) {
-  console.error(errorText(`Invalid command: ${args[0]}`));
+  console.error(errorText(`Invalid command: ${parsed.positionals[0]}`));
   console.error(errorText(`Currently only ${migrationCommandName} is supported.`));
   console.info(usage);
   process.exit(1);
@@ -36,20 +54,26 @@ if (typeof migrationName !== "string") {
 }
 
 const timestamp = Date.now();
-const migrationFilePath = join(migrationDir, `${timestamp}-${migrationName}.ts`);
-
-const migrationFileContent = [
-  `import { Kysely, Migration } from "kysely";`,
-  ``,
-  `export class ${migrationName}${timestamp} implements Migration {`,
-  `  public async up(db: Kysely<unknown>): Promise<void> {`,
-  `  }`,
-  `  public async down(db: Kysely<unknown>): Promise<void> {`,
-  `  }`,
-  `}`,
-];
+const migrationFilePath = join(migrationDir, `${timestamp}-${migrationName}.${parsed.values.type}`);
 
 mkdirSync(migrationDir, { recursive: true });
-writeFileSync(migrationFilePath, migrationFileContent.join("\n"));
+
+if (parsed.values.type === "ts") {
+  const migrationFileContent = [
+    `import { Kysely, Migration } from "kysely";`,
+    ``,
+    `export class ${migrationName}${timestamp} implements Migration {`,
+    `  public async up(db: Kysely<unknown>): Promise<void> {`,
+    `  }`,
+    parsed.values["no-down"]
+      ? undefined
+      : [`  public async down(db: Kysely<unknown>): Promise<void> {`, `  }`].join("\n"),
+    `}`,
+  ].filter((x) => x);
+
+  writeFileSync(migrationFilePath, migrationFileContent.join("\n"));
+} else {
+  writeFileSync(migrationFilePath, "");
+}
 
 console.info(`Created migration file: ${relative(process.cwd(), migrationFilePath)}`);
